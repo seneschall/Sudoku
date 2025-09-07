ad66141ebf950c0b70baf8c5dfbf68f2ae57f29d
Simon Sack <simon@p200300cebf10d91a8edd4af5ee134f7e.dip0.t-ipconnect.de>
2025-09-07 11:03:18 +0200

    refactor

--- sudoku.py
+++ sudoku.py
@@ -20,49 +20,28 @@ def is_in_column_of(grid: GameGrid, x: int, val: int) -> bool:
 
 
 def is_in_region_of(grid: GameGrid, x: int, y: int, val: int) -> bool:
-    pos: list[int] = [-1, -1]
-    TOP = 0
-    BOTTOM = 1
-    CENTRE = 2
-    LEFT = 0
-    RIGHT = 1
-
     # where in the region is x? Left, right, or center?
     # could be refactored using `x % 3` but I'll keep it as is for legibility
-    if x == 0 or x == 3 or x == 6:
-        pos[0] = LEFT
-    elif x == 1 or x == 4 or x == 7:
-        pos[0] = CENTRE
-    else:
-        pos[0] = RIGHT
-
-    # where is y?
-    if y == 0 or y == 3 or y == 6:
-        pos[1] = TOP
-    elif y == 1 or y == 4 or y == 7:
-        pos[1] = CENTRE
-    else:
-        pos[1] = BOTTOM
-
-    # Which values do we need to consider?
-    if pos[0] == LEFT:
+    if x == 0 or x == 3 or x == 6:  # i.e. is x in left field of region?
         xs = [x, x + 1, x + 2]
-    elif pos[0] == CENTRE:
+    elif x == 1 or x == 4 or x == 7:  # i.e. is x in centre field of region?
         xs = [x - 1, x, x + 1]
-    else:
+    else:  # i.e. is x in right field of region?
         xs = [x, x - 1, x - 2]
 
-    if pos[1] == TOP:
+    # where is y?
+    if y == 0 or y == 3 or y == 6:  # i.e. is y in top field of region?
         ys = [y, y + 1, y + 2]
-    elif pos[1] == CENTRE:
+    elif y == 1 or y == 4 or y == 7:  # i.e. is x in centre field of region?
         ys = [y - 1, y, y + 1]
-    else:
+    else:  # i.e. is x in right field of region?
         ys = [y, y - 1, y - 2]
 
     for yn in ys:
         for xn in xs:
             if grid[yn][xn] == val:
                 return True
+
     return False
 
 
