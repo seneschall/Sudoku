d9ed0a9b351346f9bb2880786e5cdfa1ebca5c25
Simon Sack <simon@p200300cebf10d91a8edd4af5ee134f7e.dip0.t-ipconnect.de>
2025-09-07 21:05:51 +0200

    implemented basic frontend for testing

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import (
     Flask,
+    redirect,
     # redirect,
     render_template,
     # render_template_string,
@@ -8,13 +9,25 @@ from flask import (
     # Response,
 )
 
+from sudoku import make_game
+
 app = Flask(__name__)
+
+grid: list[list[int]] = []
+
+
+@app.route("/generate", methods=["GET", "POST"])
+def gen():
+    global grid
+    grid = make_game(20)
+    return redirect("/")
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    return render_template("index.html", grid=grid)
 
 
 if __name__ == "__main__":
+    grid = make_game(20)
     app.run(debug=True)
